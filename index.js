6a68337a3a3acc0832e715440f13bf8563b3de13
Enzo de Villarreal <enzo.devillarreal.contractor@bbva.com>
2025-01-02 09:50:05 +0100

    feat: update list of team members for 25Q1 SP1

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ import Buzon from './buzon.js';
 
 (function() {
   const persons = [
-    { name: 'German', avatar: './img/pacman2.png' },
-    { name: 'Miguel Ángel', avatar: './img/pacman2.png' },
-    { name: 'Julio', avatar: './img/pacman2.png' },
-    { name: 'Jesús', avatar: './img/pacman2.png' },
     { name: 'Enzo', avatar: './img/pacman2.png' },
+    { name: 'Gonzalo', avatar: './img/pacman2.png' },
+    { name: 'Samu', avatar: './img/pacman2.png' },
   ];
 
   const $mainImage = document.querySelector('#main-image');
