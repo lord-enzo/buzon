ff9c894bf0e2f2b50f98276a092ce6fd17eeb239
Enzo de Villarreal <enzo.devillarreal.contractor@bbva.com>
2025-11-20 11:22:42 +0100

    feat: add Tony to rotation

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import Buzon from './buzon.js';
     { name: 'Miguel', avatar: './img/pacman2.png' },
     { name: 'Luis', avatar: './img/pacman2.png' },
     { name: 'Gonzalo', avatar: './img/pacman2.png' },
+    { name: 'Tony', avatar: './img/pacman2.png' },
   ];
 
   const $mainImage = document.querySelector('#main-image');
